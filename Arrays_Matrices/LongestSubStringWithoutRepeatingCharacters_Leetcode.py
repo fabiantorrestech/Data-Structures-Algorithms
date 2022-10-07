8c29bf0edd2ce38ed9bf7527a744b89474e5ebf4
Fabian <fables@Acer-Swift-X-2021.localdomain>
2022-10-06 19:40:30 -0500

    longest substring without repeating characters added - leetcode - medium - python - sliding window problem - completed with a python set data-structure

---
+++ Arrays_Matrices/LongestSubStringWithoutRepeatingCharacters_Leetcode.py
@@ -0,0 +1,20 @@
+#problem: https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        seenChars = set()
+        left = 0
+        longestSubstr = 0
+        
+        for right in range(len(s)):
+            while s[right] in seenChars:    # we've detected repeated character thats same as right ptr
+                seenChars.remove(s[left])  # - delete the duplicate from set. it will stil appear in set
+                                            #   until duplicate has been deleted.
+                left += 1
+            
+            seenChars.add(s[right])         # add unseen character to set
+            windowSize = (right-left)+1
+            longestSubstr = max(longestSubstr, windowSize)
+            
+        return longestSubstr
+        
