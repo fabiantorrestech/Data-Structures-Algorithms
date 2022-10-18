be705b4b6aa23bae664f3409fc5a3b2b906d5843
Fabian Torres <fabiantorrestech@gmail.com>
2022-10-18 12:17:11 -0500

    added 2 java files -- reading input and printing with scanner in java. HackerRank StdinAndStdoutI in java.

---
+++ Language_Basics/Java Basics/ReadingInput_PrintingOutput.java
@@ -0,0 +1,16 @@
+import java.util.Scanner; // utilizing Java Scanner for input
+
+public class Main
+{
+	public static void main(String[] args) {
+		
+	    Scanner scan = new Scanner(System.in); 
+
+	    int a = scan.nextInt();
+	    String b = scan.next();
+	    
+	    
+	    System.out.println("a: " + a);
+		System.out.println("b: " + b);
+	}
+}
