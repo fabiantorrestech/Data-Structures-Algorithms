f6497990df8aa75035140640a6440c94562a898c
Fabian Torres <fabiantorrestech@gmail.com>
2022-10-19 13:12:26 -0500

    added maxAreaOfIslandcompleted completed solution -- method1 with no extra memory, modifying map though

--- Arrays_Matrices/maxAreaOfIsland_leetcode.py
+++ Arrays_Matrices/maxAreaOfIsland_leetcode.py
@@ -1,5 +1,6 @@
 # problem: https://leetcode.com/problems/max-area-of-island/
-# ---- NOT FINISHED YET --- 
+# method1 -  no extra memory needed (O(m*n)), m=numRows, n=numColumns
+
 
 class Solution:
     
@@ -12,27 +13,24 @@ class Solution:
         
     def explore(self, grid: List[List[int]], i: int, j: int) -> int:
         
-        if i<0 or i>=len(grid) or j<0 or j>=len(grid[i]) or grid[i][j]==0: # boundary checks / if its water (0)
+        if i<0 or i>=len(grid) or j<0 or j>=len(grid[i]) or grid[i][j]==0 or grid[i][j]==-1: # boundary checks / if its water (0) / already been visited (-1)
             return 0
         
-        print("i: ", i, "j: ", j, "\n")
-        totalArea=1
         grid[i][j] = -1                       # - mark the current land spot as 'explored' 
-        totalArea+=self.explore(grid, i+1, j) # up
-        totalArea+=self.explore(grid, i-1, j) # down
-        totalArea+=self.explore(grid, i, j-1) # left
-        totalArea+=self.explore(grid, i, j+1) # right
-        
+        totalArea = 1
+        totalArea += self.explore(grid, i+1, j) # up
+        totalArea += self.explore(grid, i-1, j) # down
+        totalArea += self.explore(grid, i, j-1) # left
+        totalArea += self.explore(grid, i, j+1) # right
         return totalArea
     
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        
+        maxArea = 0
         for i in range(len(grid)):
             for j in range(len(grid[i])):
-                if grid[i][j] == 1:
-                    currArea = self.explore(grid, i, j)
-            
-            maxArea = max(currArea, maxArea)
+                if grid[i][j] == 1:                     # we've hit land
+                    currArea = self.explore(grid, i, j) # calculate currIslandArea via DFS-recursion
+                    maxArea = max(currArea, maxArea)    # is currIslandArea > maxIslandArea?
                 
-        
         return maxArea
+        
