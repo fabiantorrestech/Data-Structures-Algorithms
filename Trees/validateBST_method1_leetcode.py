57c35196c27e784660ae6e737986da2a6a5e2ac0
Fabian Torres <fabiantorrestech@gmail.com>
2022-11-01 19:01:32 -0500

    validateBST medium from leetcode added -- still understanding/taking notes

---
+++ Trees/validateBST_method1_leetcode.py
@@ -0,0 +1,28 @@
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    
+    
+    def recValidBST(self, curr: Optional[TreeNode], leftBoundary: int, rightBoundary: int) -> bool:
+        if not curr:                                                    # base case / empty tree is a valid BST.
+            return True
+        
+        if curr.val <= leftBoundary or curr.val >= rightBoundary:       # leftBoundary < curr < rightBoundary
+            return False
+        
+        left, right = curr.left, curr.right
+        leftResult = self.recValidBST(left, leftBoundary, curr.val)     # traverse left child, update rightBoundary.
+        rightResult = self.recValidBST(right, curr.val, rightBoundary)  # traverse right child, update leftBoundary.
+        
+        if leftResult and rightResult:                                  # left and right must be valid to be a valid BST
+            return True
+    
+        return False
+    
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        return self.recValidBST(root, float("-inf"), float("inf"))      # solve via recursion
