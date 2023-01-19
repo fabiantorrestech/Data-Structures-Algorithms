faf1fbbc823570b2ef5e7caab5fb2fb60f4e92b1
Fabian Torres <fabiantorrestech@gmail.com>
2023-01-19 11:31:08 -0600

    c cpp tips

---
+++ self-made-tools-for-dsa/c++/InitializeAHashmap.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int main(){
+
+    // declaration
+    unordered_map<string, int> hashmap;
+
+    // insertion
+    hashmap["key1"] = 1;
+    hashmap["key2"] = 2;
+    hashmap["key3"] = 3;
+
+    // printing a hashmap's keys and values
+    // - x.first = key
+    // - x.second = value
+    for(auto x : hashmap){
+        cout << "hashmap[" << x.first << "]: " << x.second << endl; 
+    }
+
+    // lookup by key
+
+    // delete key
+    
+
+
+    return 0;
+}
---
+++ self-made-tools-for-dsa/c/ArgC_ArgV.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+
+int main(int argc, char* argv[]){
+    
+    printf("argc: %d\n", argc);
+    for(int i=0; i<argc; i++){
+        printf("argv[%d]: %s\n", i, argv[i]);
+    }
+    
+    return 0;
+}
